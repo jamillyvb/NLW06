1a8bd2ab688f0814ed09ecbdbacc3ba67dfc302e
Jamilly Barbosa <jamillyvitorya14@gmail.com>
2021-06-23 19:39:53 -0300

    terceira aula completa

--- src/controllers/CreateTagController.ts
+++ src/controllers/CreateTagController.ts
@@ -2,9 +2,17 @@ import { Request, Response } from "express";
 import {CreateTagService} from "../services/CreateTagService";
 
 class CreateTagController {
-    async handle(request: Response, response: Response){
-        
+
+    async handle(request: Request, response: Response){
+       
+        const { name } = request.body;
+        //chamar service
+        const createTagService = new CreateTagService();
+
+        const tag = await createTagService.execute(name);
+
+        return response.json(tag);
     }
 }
 
-export { CreateTagController }
+export { CreateTagController };
